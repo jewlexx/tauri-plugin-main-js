eb5c13fbaa6d32cd0b9c4b76803bc85f28ca6ea1
Juliette Cordor <grassojames5@gmail.com>
2022-09-18 11:42:01 +1000

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,15 @@ use tauri::{
     Runtime,
 };
 
+pub(crate) use deno_core::include_js_files;
+
+#[macro_export]
+macro_rules! include_main {
+    ($path:literal) => {
+        $crate::include_js_files($path)
+    };
+}
+
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("main-js").build()
